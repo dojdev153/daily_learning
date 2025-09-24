7be7d68d9d8668112009f226f161d67b163786ce
dojdev153 <hitayezufrank@gmail.com>
2025-09-24 20:52:11 +0200

    another one

--- DAY_07/SPACE_SHOOTER/code/main.py
+++ DAY_07/SPACE_SHOOTER/code/main.py
@@ -1,10 +1,38 @@
 import pygame
+import random
 
 #general setup
 pygame.init()
 w,h = 1280,720
 screen = pygame.display.set_mode((w,h))
+pygame.display.set_caption("space shooter")
+icon = pygame.image.load("../images/logo.png")
+pygame.display.set_icon(icon)
 running = True
+
+#plain surface
+surface = pygame.Surface((100,200))
+surface.fill("orange")
+ship_x= 100
+
+#importing an image
+img = pygame.image.load("../images/space.png").convert_alpha()
+img = pygame.transform.scale(img,(80,80))
+
+#importing the star
+star = pygame.image.load("../images/star.png").convert_alpha()
+star = pygame.transform.scale(star,(100,100))
+star.set_alpha(100)
+
+#generate random star positions
+stars=[]
+for i in range(20):
+    x = random.randint(0,w)
+    y = random.randint(0,h)
+    alpha = random.randint(50,200)
+    star_copy = star.copy()
+    star_copy.set_alpha(alpha)
+    stars.append((x,y,star_copy))
 
 while running:
     #event loop
@@ -12,8 +40,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #draw the game
-    screen.fill("red")
+#draw the game
+    #background
+    screen.fill("black")
+    #stars
+    for(x,y,s) in stars:
+        screen.blit(s,(x,y))
+    #ship
+    ship_x +=0.1
+    screen.blit(img,(ship_x,200))
     pygame.display.flip()
 
 pygame.quit()
