67ae17a9ecddf2acfa46d1d344250c147973cb29
dojdev153 <hitayezufrank@gmail.com>
2025-10-17 11:22:29 +0200

    another on

--- DAY_21/Formatting.java
+++ DAY_21/Formatting.java
@@ -1,3 +1,10 @@
+
 public class Formatting {
-    
+    public static void main(String[] args) {
+        System.out.printf("%b", 4 < 5);
+        System.out.printf("%.3f%n", 24.67890);
+        System.out.printf("%-10s|%-5s%n","Name","Age");
+        System.out.println("---------------------%n");
+        System.out.printf("%-10s|%-5s%n","Nikita",17);
+    }
 }
