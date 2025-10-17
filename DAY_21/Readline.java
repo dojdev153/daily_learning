2b1227e11e5e59a69adadab3d7c21071e8dcdc92
dojdev153 <hitayezufrank@gmail.com>
2025-10-17 09:15:28 +0200

    another one

---
+++ DAY_21/Readline.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Readline {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line;
+        System.out.println("enter your inputs, type");
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            if(line.equalsIgnoreCase("exit"))
+               break;
+            System.out.println("you entered : " + line);
+        }
+        
+    }
+}
