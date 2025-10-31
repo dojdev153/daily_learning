c9b8c297a2ebb5d52c196c7c11da990e13487205
dojdev153 <hitayezufrank@gmail.com>
2025-11-01 00:45:35 +0200

    another one

---
+++ DAY_26/types/GenericType2.java
@@ -0,0 +1,32 @@
+package DAY_26.types;
+import java.util.*;
+
+class Pair<K, V>{
+    K key;
+    V value;
+
+    Pair(K Key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+}
+
+class Data<T extends Number>{
+    T num;
+    Data(T num){
+        this.num = num;
+    }
+    double square(){
+        return num.doubleValue() * num.doubleValue();
+    }
+}
+public class GenericType2 {
+    public static void main(String[] args) {
+        Pair<String, Integer> Pairs = new Pair<>("alice", 85);
+        System.out.println(Pairs.key + " "+ Pairs.value);
+
+        Data<Integer> d1 = new Data<>(5);
+        System.out.println(d1.square());
+    }
+}
