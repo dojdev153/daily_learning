f229fddb93504119a0a554398ce6a10ade7d4c53
dojdev153 <hitayezufrank@gmail.com>
2025-10-31 12:11:00 +0200

    another one

---
+++ DAY_26/types/LowerboundWildcardDemo.java
@@ -0,0 +1,26 @@
+package DAY_26.types;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LowerboundWildcardDemo {
+    static void addSomeInt(List< ? super Integer> list){
+        list.add(100);
+        list.add(200);
+        list.add(300);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> ints = new ArrayList<>();
+        List<Number> nums = new ArrayList<>();
+        List<Object> objs = new ArrayList<>();
+
+        addSomeInt(ints);
+        addSomeInt(nums);
+        addSomeInt(objs);
+
+        System.out.println("ints: " + ints);
+        System.out.println("nums: " + nums);
+        System.out.println("objs: " + objs);
+    }
+}
