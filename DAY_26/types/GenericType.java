638a66bbe4909f46abac56ea180ff01609816134
dojdev153 <hitayezufrank@gmail.com>
2025-11-01 00:29:01 +0200

    another one

---
+++ DAY_26/types/GenericType.java
@@ -0,0 +1,30 @@
+package DAY_26.types;
+import java.util.*;
+
+class Box<T>{
+    private T value;
+    void set(T value){
+        this.value = value;
+    }
+    T get(){
+        return value;
+    }
+    public static <T> void display(T item){
+        System.out.println(item);
+    }
+
+}
+
+public class GenericType {
+    public static void main(String[] args) {
+        Box<String> nameBox = new Box<>();
+        nameBox.set("Box_A1");
+        System.out.println(nameBox.get());
+
+        Box<Integer> numberBox = new Box<>();
+        numberBox.set(42);
+        System.out.println(numberBox.get());
+
+        Box.display("Hello world");
+    }
+}
