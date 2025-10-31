cda7ea976b056483cb446c56b2036cd851c46642
dojdev153 <hitayezufrank@gmail.com>
2025-10-31 08:01:09 +0200

    another one

---
+++ DAY_26/Collections/Intro.java
@@ -0,0 +1,13 @@
+package DAY_26.Collections;
+import java.util.*;
+public class Intro {
+    public static void main(String a[]){
+          ArrayList<String> names = new ArrayList<>();
+          names.add("Rahit");
+          names.add("Rahul");
+          String name = names.get(0);
+          System.out.println("Name: " + name);
+          System.out.println("Size: " + names.size());
+
+    }
+}
