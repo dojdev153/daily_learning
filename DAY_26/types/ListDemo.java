f815efeaa8c35aae468eb3101a68106586cc79c5
dojdev153 <hitayezufrank@gmail.com>
2025-11-01 01:40:51 +0200

    another one

--- DAY_26/types/ListDemo.java
+++ DAY_26/types/ListDemo.java
@@ -7,5 +7,16 @@ public class ListDemo {
         List<String> names = new ArrayList<>(List.of("Alice","Bob","Charlie"));
         System.out.println(names.get(0));
         System.out.println(names);
+
+        names.add(2, "lucas");
+        System.out.println(names);
+
+        names.set(1, "James");
+        System.out.println(names);
+
+        names.remove(0);
+        System.out.println(names);
+
+        System.out.println("do we have james? "+ names.contains("James"));
     }
 }
--- DAY_26/types/Lower2.java
+++ DAY_26/types/Lower2.java
@@ -1,5 +1,26 @@
 package DAY_26.types;
+import java.util.*;
 
+class Box<T>{
+    private T value;
+    public Box(){
+        this.value = value
+    }
+
+    public getValue(){
+        return value
+    }
+    public setValue(T value){
+        this.value = value;
+    }
+    public void processWild(List<? super Number> box){
+
+    }
+}
 public class Lower2 {
-    
+    public static void main(String[] args) {
+        Box<Object> nameBox = new Box<>("Box_A1");
+        Box<Number> numBox = new Box<>(3);
+
+    }
 }
