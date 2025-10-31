debe71a6d5e6904f13eb5297d498761d6b7b90e6
dojdev153 <hitayezufrank@gmail.com>
2025-10-31 12:04:58 +0200

    another one

---
+++ DAY_26/types/UpperboundWildcardDemo.java
@@ -0,0 +1,20 @@
+package DAY_26.types;
+import java.util.*;
+
+public class UpperboundWildcardDemo {
+    static double sumNumbers(List<? extends Number> nums){
+        double sum = 0.0;
+        for(Number n : nums){
+            sum += n.doubleValue();
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> ints = Arrays.asList(10,20,30);
+        List<Double> doubles = Arrays.asList(1.5,2.5,3.0);
+
+        System.out.println("sum of ints: " + sumNumbers(ints));
+        System.out.println("sum of doubles: " + sumNumbers(doubles));
+    }
+}
